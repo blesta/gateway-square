897206402573c1d9a76a5a637f9bf0e7be331dc9
Jonathan Reissmueller <jonathan@phillipsdata.com>
2021-09-24 10:20:33 -0700

    Updated Verify Host on cURL call

--- lib/square_api.php
+++ lib/square_api.php
@@ -66,7 +66,7 @@ class SquareApi
 
         if (Configure::get('Blesta.curl_verify_ssl')) {
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
         } else {
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
